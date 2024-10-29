711d801c874e9d732339ef0718e600a9d3b3cd4f
APS-CS <qhapaq@hotmail.com>
2024-10-29 12:06:29 +0100

    Löste uppgift 2

--- Uppgifter_kap2.py
+++ Uppgifter_kap2.py
@@ -1,3 +1,4 @@
+#uppgift 1
 namn = " aNnA kaRlSsOn "
 
 #V1
@@ -13,3 +14,11 @@ resultat = "-".join(dela_ord)
 
 print(resultat)
 
+#uppgift 2
+order = ("bröd", "mjölk", "ägg", "smör", "ost", "yoghurt")
+
+print(order[0:3])
+print(order[-2:])
+print(order[::2])
+
+#uppgift 3
