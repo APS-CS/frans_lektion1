b0401f3e390067b89e6cd5551231bec183447d8d
APS-CS <qhapaq@hotmail.com>
2024-10-29 11:50:55 +0100

    Löste uppgift 1

--- Uppgifter_kap2.py
+++ Uppgifter_kap2.py
@@ -1,1 +1,15 @@
 namn = " aNnA kaRlSsOn "
+
+#V1
+format_ord = namn.strip().title()
+dela_ord = format_ord.split()
+
+print(dela_ord[0]+"-"+dela_ord[1])
+
+#V2
+format_ord = namn.strip().title()
+dela_ord = format_ord.split()
+resultat = "-".join(dela_ord)
+
+print(resultat)
+
