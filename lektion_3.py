31054285d81fc3aca9a0d39a87b33504e8a0d56d
APS-CS <qhapaq@hotmail.com>
2024-10-31 16:30:43 +0100

    Under lektion 3

---
+++ lektion_3.py
@@ -0,0 +1,34 @@
+# x = 10
+# if x > 5:
+#     print("x is greater than 5")
+
+# if x > 15:
+#     print("X is greater than 15")
+
+# if condition1:
+
+# elif condition2:
+
+# x = 10
+# if x > 15:
+#     print("X is greater than 15")
+
+# elif x > 5:
+#     print("X is greater than 5")
+
+# x = 3
+# if x > 15:
+#     print("x is greater than 15")
+# elif x > 5:
+#     print ("x is greater than 5")
+# else:
+#     print("x is less than 5")
+
+# x = 20
+# if x > 10:
+#     print("Above ten")
+#     if x > 20:
+#         print ("and also above 20!")
+#     else:
+#         print("but not above 20.")
+
