6897e14fef690b189a655e035ca0c8648e4a2be4
APS-CS <qhapaq@hotmail.com>
2024-11-06 09:34:06 +0100

    Kap 5 avslutas, kap 6 börjar

---
+++ Applied Script/Lektion6.py
@@ -0,0 +1,32 @@
+######## Installera PIP ######## - KAPITEL 5
+
+##fungerar för att installera ett paket med funktionaliteter
+
+## pyfiglet - skämtpaket med fonts
+
+# import pyfiglet
+
+# # 
+
+
+# ## colorama - färgad font i terminal
+# # kommando som man inte bara vet, utan får kolla upp
+
+# from colorama import init, Fore, Back, Style
+
+# init()
+# print(Fore.RED + "Den här texten är röd")
+# print(Fore.GREEN + "Den här texten är grön")
+
+# # print(Back.RED + "Röd bakgrund") #måste avsluta formateringen annars forsätter skiten att gälla
+# print(Back.RED + "Röd bakgrund" + Style.RESET_ALL)
+
+# print(Fore.RED + "Warning: Error")
+
+# import pyfiglet
+# ascii_art = pyfiglet.figlet_format("Amperdamper")
+# print(Back.WHITE + Fore.RED + ascii_art + Style.RESET_ALL)
+
+
+######## OOP ######## - KAPITEL 6
+
