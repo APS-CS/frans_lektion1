7c91721f36c13adc37082b217389fe82f61b2927
APS-CS <qhapaq@hotmail.com>
2024-11-07 14:53:39 +0100

    börjar förstå argument :)

--- Applied Script/Kap6/argparse_lek.py
+++ Applied Script/Kap6/argparse_lek.py
@@ -1,32 +1,36 @@
-# #### adderare i terminal #####
+#### adderare i terminal #####
 
-# import argparse                         #### krav 1 - importera argparse
+import argparse                         #### krav 1 - importera argparse
 
-# parser = argparse.ArgumentParser()      #### krav 2 - använd 
+parser = argparse.ArgumentParser()      #### krav 2 - använd 
 
-#     #### positional arguments! Dessa måste skrivas i för att gå vidare
-# parser.add_argument("greeting", help="Hälsning visas")
-# parser.add_argument("-n", "--numbers", type=int, nargs=2, help="Skriv in nummer som ska adderas")
-#     #### n = numbers kategori, type = typ av inmatning, nargs = number of arguments
+    #### positional arguments! Dessa måste skrivas i för att gå vidare
+parser.add_argument("greeting", help="Hälsning visas")
 
-
-
-
-# args = parser.parse_args()              #### krav 3 - nu kan man skriva ut pars
-
-# # print(args)               #### printar alla argument som finns
-# print(args.greeting)      #### skriver tillbaka användarens hälsning
-# # print(args.numbers)       #### printar argument inom given kategori
+    ###optional arguments! Dessa behöver inte fyllas i
+parser.add_argument("-n", "--numbers", type=int, nargs=2, help="Skriv in nummer som ska adderas") ####Notera -n och --numbers
+parser.add_argument("-b", "--Blubb_blubb", help="Gör inget men vill ha värde ett värde trolololo :)")  #### Gör inget men matar man in -a i temrinalen så förväntar den sig ett värde
 
 
 
-# if args.numbers is not None:
-#     print(f"{args.numbers[0]} + {args.numbers[1]}")
-#     result = (args.numbers[0] + args.numbers[1])
-#     print("Resultat: ", result)
 
 
-import argparse
+args = parser.parse_args()              #### krav 3 - nu kan man skriva ut pars
 
-parser = argparse.ArgumentParser()
+# print(args)               #### printar alla argument som finns
+print(args.greeting)      #### skriver tillbaka användarens hälsning
+# print(args.numbers)       #### printar argument inom given kategori
 
+
+
+if args.numbers is not None:
+    print(f"{args.numbers[0]} + {args.numbers[1]}")
+    result = (args.numbers[0] + args.numbers[1])
+    print("Resultat: ", result)
+
+
+# import argparse
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument()
