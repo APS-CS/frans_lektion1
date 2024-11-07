786708a9cf38fb6f3d330bd61ffb2dc876ecda8c
APS-CS <qhapaq@hotmail.com>
2024-11-07 18:50:14 +0100

    Lektion 7

--- Lektion7.py
+++ Lektion7.py
@@ -7,7 +7,7 @@
 
 
 # #### Skapa nyckel ####
-# from cryptography.fernet import Fernet              ####importerar Key
+from cryptography.fernet import Fernet              ####importerar Key
 
 # key = Fernet.generate_key()                         #### definerar variabel som anropar funktion i standard biblioteket som generar nyckel
 
@@ -27,6 +27,45 @@
 # print(f"Key is loaded: {key.decode()}")
 
 # #### Går även att skriva in i terminalen och få fram det där #####
-# #### cat secret.key #####
+# #### type secret.key #####
 
 
+######## Kryptera ett meddelande ########
+print("Här krypterar vi:")
+
+#### laddar nyckeln
+with open("secret.key", "rb") as key_file:
+    key = key_file.read()
+print(f"Key is loaded: {key.decode()}")                     #### Bra med att lägga in printar för att veta vart/om koden går fel
+
+#### skapar instans av nyckeln
+cipher_suite = Fernet(key)
+print("Fernet object is created and ready for encryption")
+
+#### krypteringen av texten 
+
+message = "This is a string with a secret message".encode() #### Funktionen encrypt finns i FERNET därav "odefinerad" funktion - Se nedre rad
+cipher_text = cipher_suite.encrypt(message)                 #### får in texten och gör om till bytes 
+print(f"Encrypted message: {cipher_text}")                  #### kontroll
+
+#### spara texten till en fil
+
+with open("encrypted_message.enc", "wb") as enc_file:
+    enc_file.write(cipher_text)
+
+print("Encrypted message is saved to file 'encrypted_message.enc'")
+print("\n")
+
+######## decrpytera
+print("Här dekrypterar vi:")
+
+with open("encrypted_message.enc", "rb") as enc_file:
+    encrypted_message = enc_file.read()
+
+print(f"Encrypted message is loaded: {encrypted_message}")
+
+plain_text = cipher_suite.decrypt(encrypted_message)
+print(f"Decrypted message: {plain_text.decode()}")
+
+
+#### Uppgiften: det vi ska göra är att kryptera en fil ist för en message/sträng
