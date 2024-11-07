4034c6f978d835c0f8d2fa75b5a5bf513ddecd1f
APS-CS <qhapaq@hotmail.com>
2024-11-07 10:13:36 +0100

    Kap3 uppg3 - klar

--- Applied Script/Kap3/Kap3_uppg3.py
+++ Applied Script/Kap3/Kap3_uppg3.py
@@ -47,22 +47,39 @@
 ## om True eller false så skriv ut i terminal
 ## om true eller false skriv till ny fil
 
+import os
 
-# Lista över användarnamn och deras korrekta lösenord
-user_credentials = {
-    "user1": "Password123",
-    "admin": "Admin@2023",
-    "user2": "Welcome123",
-    "guest": "Guest1234"
-}
-
-password_list = ["Password123", "123456", "Welcome123", "Guest1234", "password"] 
+def create_file(filename):
+    if not os.path.exists(os_filename):
+        with open(os_filename, "w"):
+            print(f"Fil {filename} skapades")
 
 
-for data in user_credentials.items():
-    for password in password_list:
-        if password in data:
-            print(f"{data} -> Great success")
-        else:
-            print(f"{data} -> failed")
-            break
+
+while True:
+    filename = input("Skapa fil med anv och lösen: ")
+    os_filename = filename+".txt"
+    create_file(filename)
+
+
+    # Lista över användarnamn och deras korrekta lösenord
+    user_credentials = {
+        "user1": "Password123",
+        "admin": "Admin@2023",
+        "user2": "Welcome123",
+        "guest": "Guest1234"
+    }
+
+    password_list = ["Password123", "123456", "Welcome123", "Guest1234", "password"] 
+
+    with open(os_filename, "w") as os_filename:   ##gör w för att inte hålla på behöva skapa kontroll av ny fil, kanske gör om i framtiden
+        for user, userpassword in user_credentials.items():
+            for password in password_list:
+                    if password in userpassword:
+                            os_filename.write(f"{user}: {password} -> Great success\n")
+                            print(f"{user}: {password} -> Great success")
+
+                    if password not in userpassword:
+                            os_filename.write(f"{user}: {password} -> Failed\n")
+                            print(f"{user}: {password} -> Failed")
+        break
--- Applied Script/Kap3/password_spraying.py
+++
@@ -1,7 +0,0 @@
-# Lista över användarnamn och deras korrekta lösenord
-user_credentials = {
-    "user1": "Password123",
-    "admin": "Admin@2023",
-    "user2": "Welcome123",
-    "guest": "Guest1234"
-}
