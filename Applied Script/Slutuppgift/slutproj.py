b4eae0d7384d7ba9f30f1404b84fff2c722e321c
APS-CS <qhapaq@hotmail.com>
2024-11-08 10:52:57 +0100

    Mer frågor kommer upp, skriver i kommentarplanering

--- Applied Script/Slutuppgift/slutproj.py
+++ Applied Script/Slutuppgift/slutproj.py
@@ -1,4 +1,4 @@
-######## Projekt Frans Schartaus ########
+################ Projekt Frans Schartaus ################
 
 ## Skapa ett verktyg som kan:
 
@@ -22,13 +22,42 @@
 
 ## Dekryptera en krypterad fil och återställa originalet.
 
-#### Engna anteckningar:
+################ Plan för uppgiften ################
+
+#### Fernet
 ## skapa skript som gör krypteringsprocedur, ha samma fil som avkrypterar
-## Det ska vara argparse som tar in fil i skripten och kör bestämd kommando.
+
+## skapa funktion för att skapar nyckel - CHECK
+## skapa funktion för att kryptera
+## skapa funktion för att dekryptera
+
+
+#### FRÅGOR: Hur göra om flera filer? Krypera med bara en nyckel? Skapa en ny nyckel varje gång? Kan det bli dubbelkryp?
+
+
+#### Argparse
+## Det ska vara argparse som tar in fil i skripten krypterar/dekrypterar
+
+## 1. Krypera
+## 1a. Ange fil -> Bekräftelse
+## 1a. Ange fil -> Fil redan kryperad?
+
+## 2. Dekryptera
+## 2a. Ange fil -> Bekräftelse
+## 2a. Ange fil -> Fil redan kryperad? 
+
+## 3. Skapa nyckel -> Bekräftelse
+
+
+#### FRÅGOR: Hur ange filnamn som ska de/krypteras? 
+
 
 #### Om tid finns: 
 ## skapa (i skripten) funktion för felmeddelande, tex om filen redan är krypterad eller om filen redan av avkrypterad
 ## köra skripten och skapa ny nyckel
+
+
+################ Ain't nuttin but to do it!! ################
 
 ##importera argparse
 import argparse
@@ -37,16 +66,18 @@ parser = argparse.ArgumentParser()
 ##importera Fernet
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
-print("Nyckel skapat - great success!")
+#### funktion för at skapa nyckel
+def generate_key_mode(newkey):                                          ####funktionen skapar ny nykel.. hur använda en nyckel som är redan skapad?
+    newkey = Fernet.generate_key()
+    print("Nyckel skapat i RAM - great success! \n")                    ####Bekräftelse
 
-with open("crypto_key.key", "wb") as cf:
-    ck.write(key)
+    with open("crypto_key.key", "wb") as key_file:                      ####Förvald crypto_key.key
+        key_file.write(newkey)
+        print("Nyckel skapat i fil - great success! \n")                ####Bekräftelse
 
 
+#### funktion för att kryptera fil
+with open("crypto_key.key, rb") as key_file:
+    key = key_file.read()                                               #### öppnar förvald crypto_key.key                            
+    print("Nyckel skapat i RAM - great success! \n")                    #### Bekräftelse
 
-# ## öppnar med nyckeln
-# with open("crypto_key.key", "wb") as file:
-#     key = file.read()
-
-# cipher_suite = Fernet(key)
