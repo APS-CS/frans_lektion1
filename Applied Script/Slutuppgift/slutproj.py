752e01f4eea74ba855111e12dc0103f3011b9f34
APS-CS <qhapaq@hotmail.com>
2024-11-10 15:29:04 +0100

    Kan nu välja fil och välja nyckel

--- Applied Script/Slutuppgift/slutproj.py
+++ Applied Script/Slutuppgift/slutproj.py
@@ -76,27 +76,27 @@ from cryptography.fernet import Fernet
 #### funktion för att skapa nyckel
 def generate_key_mode(name_key_in_gkm):                                                #### funktionen skapar ny nykel.. hur använda en nyckel som är redan skapad?
     newkey = Fernet.generate_key()
-    print("Nyckel skapat i RAM - great success! \n")                            #### Bekräftelse
+    print(f"Nyckel {newkey} skapat i RAM - great success! \n")                            #### Bekräftelse
     with open(name_key_in_gkm, "wb") as key_file:                                      #### kan nu spara ny nyckel
         key_file.write(newkey)
         print("Nyckel skapat i fil - great success! \n")                        #### Bekräftelse
 
 
 #### funktion för att kryptera fil
-def encrypt_and_store_info():
-    with open(name_key, "rb") as key_file:                                       #### Öppnar med vald nyckel
+def encrypt_and_store_info(file, key):
+    with open(key, "rb") as key_file:                                       #### Öppnar med vald nyckel
         key = key_file.read()                                                                               
-        print(f"Nyckel {name_key} öppnat i RAM - success! KRYPTERING")           #### Bekräftelse
+        print(f"Nyckel {key} öppnat i RAM - success! KRYPTERING")           #### Bekräftelse
 
     cipher_suite = Fernet(key)
 
-    with open("testfil.py", "rb") as file_to_encrypt:                      
+    with open(file, "rb") as file_to_encrypt:                      
         content = file_to_encrypt.read()
         cipher_content = cipher_suite.encrypt(content)
         
-        with open("testfil.py", "wb") as file_to_encrypt:                           #### måste skriva över, går inte att använda rb+
+        with open(file, "wb") as file_to_encrypt:                           #### måste skriva över, går inte att använda rb+
             file_to_encrypt.write(cipher_content)                                   #### pröva sen med att anävnda rb+ är det plain text + cipher text som ligger i filen då?
-            print(f"Innehåll i fil: testfil.py är krypterad - GREAT SUCCESS \n")    #### Bekräftelse
+            print(f"Innehåll i fil {file} är krypterad - GREAT SUCCESS \n")    #### Bekräftelse
 
 
 #### funktion för att deryptera fil
@@ -138,14 +138,20 @@ if args.create_key:
     name_key = input("Välj nyckel: ")
     if not os.path.exists(name_key):
         generate_key_mode(name_key)
-        print("Ny nyckel skapad - Programmet stängs")                                   #### Bekräftelse
+        print("Ny nyckel skapad - Programmet stängs")                               #### Bekräftelse
     else:
         print(f"Nyckel {name_key} finns redan - Programmet stängs")  
     
+
+
 elif args.encrypt_file:
     print("encrypt_and_store_inf()- MODE AKTIVERAD\n")                              #### Bekräftelse
-    encrypt_and_store_info()
+    file = input("Välj fil: ")
+    key = input("Välj nyckel:")
+    encrypt_and_store_info(file, key)
     print("Krypterat - Programmet stängs")                                          #### Bekräftelse
+
+
 
 elif args.decrypt_file:
     print("decrypt_and_store_info()- MODE AKTIVERAD\n")                             #### Bekräftelse
