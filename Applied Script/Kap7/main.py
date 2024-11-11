e4d5b092f71f475080df9ff5f21e7c0f1cf8e089
APS-CS <qhapaq@hotmail.com>
2024-11-11 10:37:05 +0100

    Kap7 - korrigering

--- Applied Script/Kap7/main.py
+++ Applied Script/Kap7/main.py
@@ -6,7 +6,12 @@
 # print(my_module.add(10, 25))
 # print(my_module.PI)
 
-######## Importerar paket, alltså särskilda delar av en modul ########
-from my_module import greet, add
-print(greet("Bobbo"))
-print(add(26, 33))
+# ######## Importerar paket, alltså särskilda delar av en modul ########
+# from my_module import greet, add
+# print(greet("Bobbo"))
+# print(add(26, 33))
+
+from my_package import module1, module2
+
+print(module1.function1())
+print(module2.function2())
--- Applied Script/Kap7/my_package/module1.py
+++ Applied Script/Kap7/my_package/module1.py
@@ -1,4 +1,2 @@
 def function1():
     return "Function 1 från module1"
-
-#### init.py måste förstå att det är paket. tom init.py måste finnas
