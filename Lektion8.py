c19d3ab006745a6875de8302c1f954088e019f30
APS-CS <qhapaq@hotmail.com>
2024-11-11 09:24:17 +0100

    Bekräftelse på att jag tagit rätt väg

---
+++ Lektion8.py
@@ -0,0 +1,10 @@
+#### Bekräftelse att jag gjort rätt:
+#### Björn ger exempel på hur man kryterar en fil:
+#### Tänk binärt, I am a computer!
+
+# with open("bild.png", "rb") as file:
+#     data = file.read()
+
+# with open("ny_bild.png", "wb") as new_file:
+#     new_file.write(data)
+
