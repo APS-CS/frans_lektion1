d48ce641d9bf2d13fa89b19af994006db79db3a3
APS-CS <qhapaq@hotmail.com>
2024-11-12 17:02:18 +0100

    Lektion9

---
+++ Lektion9.py
@@ -0,0 +1,8 @@
+#### kör Fedora workstation - OS för att lära sig PENTEST grejer
+### lär dig powershell
+
+######## shutil ########
+import shutil
+
+shutil.copy(r"c:/AppliedScript/a.key", r"c:/AppliedScript/Applied Script/Slutuppgift/a.key.bak")
+print("File has been copied")
