ce914a5b857d0636f61ac1024dced516674e5d42
APS-CS <qhapaq@hotmail.com>
2024-11-13 16:00:26 +0100

    rensa bekräftelse prints. 2 saker kvar: description och dubbeldekrypt fel

--- Applied Script/Slutuppgift/slutprojv2.py
+++ Applied Script/Slutuppgift/slutprojv2.py
@@ -8,17 +8,14 @@ parser = argparse.ArgumentParser(description="Krypteringsverktyg 5000. exempel i
 #### funktion för att skapa nyckel
 def generate_key_mode(name_key_in_gkm):
     newkey = Fernet.generate_key()
-    print(f"Nyckel {newkey} skapat i RAM - great success! \n")                            #### Bekräftelse
     with open(name_key_in_gkm, "wb") as key_file:
         key_file.write(newkey)
-        print("Nyckel skapat i fil - great success! \n")                        #### Bekräftelse
 
 
 #### funktion för att kryptera fil
 def encrypt_and_store_info(file, key):
     with open(key, "rb") as key_file:
         key = key_file.read()                                                                               
-        print(f"Nyckel {key} öppnat i RAM - success! KRYPTERING")           #### Bekräftelse
 
     cipher_suite = Fernet(key)
 
@@ -28,14 +25,12 @@ def encrypt_and_store_info(file, key):
         
         with open(file, "wb") as file_to_encrypt:
             file_to_encrypt.write(cipher_content)
-            print(f"Innehåll i fil {file} är krypterad - GREAT SUCCESS \n")    #### Bekräftelse
 
 
 #### funktion för att deryptera fil
 def decrypt_and_store_info(file, key):
     with open(key, "rb") as key_file:
         key = key_file.read()
-        print(f"Nyckel {key} öppnat i RAM - success! DEKRYPTERING \n")         #### Bekräftelse
 
     cipher_suite = Fernet(key)
 
@@ -45,8 +40,6 @@ def decrypt_and_store_info(file, key):
         
         with open(file, "wb") as file_to_decrypt:
             file_to_decrypt.write(cipher_content)
-            print(f"Innehåll i fil: {file} är dekrypterad - GREAT SUCCESS \n")  #### Bekräftelse
-
 
 
 def main():
@@ -59,37 +52,33 @@ def main():
     args = parser.parse_args()
 
 
-
 ####  if satser
     if args.create_key:
-        print("generate_key_mode()- MODE AKTIVERAD\n")                                  #### Bekräftelse
 
         if not os.path.exists(args.create_key):
             generate_key_mode(args.create_key)
-            print(f"Nyckel {args.create_key} skapad")                               #### Bekräftelse
+            print(f"Nyckel {args.create_key} skapad")
         else:
             print(f"Nyckel {args.create_key} finns redan")
 
 
 #### check av fil + kryptering eller dekryptering
     elif args.files and args.operation:
-        print("fil hantering- MODE AKTIVERAD\n")                             #### Bekräftelse
 
         if not os.path.exists(args.files[0]):
             print(f"Fil {args.files[0]} finns ej")
-            return
         if not os.path.exists(args.files[1]):
             print(f"Nyckelfil {args.files[1]} finns ej")
             return
         else:
-            print("Crypt MODE AKTIVERAD\n")                             #### Bekräftelse
+
             if args.operation == "kryptera":
                 encrypt_and_store_info(args.files[0], args.files[1])
-                print("Filen är krypterat")                                        #### Bekräftelse
+                print(f"Filen {args.files[0]} är krypterat")
                 return
             if args.operation == "dekryptera":
                 decrypt_and_store_info(args.files[0], args.files[1])
-                print("Filen är dekrypterat") 
+                print(f"Filen {args.files[0]} är dekrypterat") 
                 return
 
 
@@ -99,8 +88,6 @@ def main():
 
     elif args.operation and not args.files:
         print("Fel: Ange '-f' och 'fil att behandla' och 'nyckelfil'")    
-
-
 
 if __name__ == "__main__":
     main()
