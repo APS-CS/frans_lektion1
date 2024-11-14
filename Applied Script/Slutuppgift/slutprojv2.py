b473b794d25ff5a746141c5db4e03642c1faf607
APS-CS <qhapaq@hotmail.com>
2024-11-14 17:16:55 +0100

    Funderar på att lägga in key args

--- Applied Script/Slutuppgift/slutprojv2.py
+++ Applied Script/Slutuppgift/slutprojv2.py
@@ -28,8 +28,10 @@ Flaggor:
                     Notera: Ange alltid filnamn först och sedan nyckelfil.
                                  
 -e  --encrypt       Välj '-e' för att kryptera den valda filen med den valda nyckelfilen
+                    Notera: En krypterad kopia med filtyp 'FILNAMN.FILTYP.enc' skapas i den aktuella sökvägen
                                  
--d  --decrypt       Välj '-d' för att dekryptera den valda filen med den valda nyckelfilen                    
+-d  --decrypt       Välj '-d' för att dekryptera den valda filen med den valda nyckelfilen
+                    Notera: En dekrypterad kopia med filanamn 'dekrypterad_FILNAMN.FILTYP' skapas i den aktuella sökvägen                    
 
                                  
 Exempel på korrekta användningar:
@@ -41,13 +43,11 @@ Kryptera en fil:    py slutprojv2.py -f testfil.txt test.key -e
 Dekryptera en fil:  py slutprojv2.py -f testfil.txt test.key -d""", 
 epilog="Information finns ovan", formatter_class=argparse.RawDescriptionHelpFormatter)   
 
-
 #### funktion för att skapa nyckel
 def generate_key_mode(name_key_in_gkm):
     newkey = Fernet.generate_key()
     with open(name_key_in_gkm, "wb") as key_file:
         key_file.write(newkey)
-
 
 #### funktion för att kryptera fil
 def encrypt_and_store_info(file, key):
@@ -64,8 +64,6 @@ def encrypt_and_store_info(file, key):
         with open(encrypted_file, "wb") as file_to_encrypt:
             file_to_encrypt.write(cipher_content)
 
-
-
 #### funktion för att deryptera fil
 def decrypt_and_store_info(file, key):
     with open(key, "rb") as key_file:
@@ -81,27 +79,21 @@ def decrypt_and_store_info(file, key):
         with open(decrypted_file, "wb") as file_to_decrypt:
             file_to_decrypt.write(cipher_content)
 
-
 def main():
 
 #### argument
     parser.add_argument("-c", "--create_key", metavar="Skapa ny nyckel", help="Skapa ny krypterinsnyckel")
-
     parser.add_argument("-f", "--files", metavar="Välj: FIL och välj: NYCKEL", nargs=2, type=str, help="Ange fil att kryptera/dekryptera och nyckelfil") 
 
     group = parser.add_mutually_exclusive_group()
-
     group.add_argument("-e", "--encrypt", action="store_true", help="Välj för kryptering")
-
-    group.add_argument("-d", "--decrypt", action="store_true", help="Välj för kryptering")
+    group.add_argument("-d", "--decrypt", action="store_true", help="Välj för dekryptering")
 
     args = parser.parse_args()
-
 
 ####  if satser
     if len(os.sys.argv) == 1:           ####fick hjälp av Yaraslau med kod rad 79-83
         parser.print_help()
-        return
 
     elif args.create_key:
 
@@ -111,13 +103,11 @@ def main():
         else:
             print(f"Nyckel {args.create_key} finns redan")
 
-
 #### check av fil + kryptering eller dekryptering
     elif args.files and args.decrypt or args.encrypt:
         if not os.path.exists(args.files[0]):
             print(f"Fil {args.files[0]} finns ej")
-            return
-        if not os.path.exists(args.files[1]):
+        elif not os.path.exists(args.files[1]):
             print(f"Nyckelfil {args.files[1]} finns ej")
             return
         
@@ -125,14 +115,11 @@ def main():
             if args.encrypt:
                 encrypt_and_store_info(args.files[0], args.files[1])
                 print(f"Filen {args.files[0]} är krypterat")
-                # return
             elif args.decrypt:
                 decrypt_and_store_info(args.files[0], args.files[1])
                 print(f"Filen {args.files[0]} är dekrypterat") 
-            return
 
-
-#### felhantering
+#### mer felhantering
     elif args.files and not args.encrypt or args.decrypt:
          print("Fel: Du behöver ange krypteringsläge '-e' eller '-d'") 
 
