a3766329b190fe1d8f929f20a1307ec37d158915
APS-CS <qhapaq@hotmail.com>
2024-11-15 15:11:42 +0100

    KLART!

--- Slutuppgift/fsh_Amos_kv.py
+++ Slutuppgift/fsh_Amos_kv.py
@@ -4,11 +4,11 @@ from cryptography.fernet import Fernet
 
 parser = argparse.ArgumentParser(description=r"""
  _  __                 _                      _    _
-| |/ /_ __ _   _ _ __ | |_    __   _____ _ __| | _| |_ _   _  __ _                                                                                                                     
-| ' /| '__| | | | '_ \| __|___\ \ / / _ \ '__| |/ / __| | | |/ _` |                                                                                                                    
-| . \| |  | |_| | |_) | ||_____\ V /  __/ |  |   <| |_| |_| | (_| |                                                                                                                    
-|_|\_\_|   \__, | .__/ \__|     \_/ \___|_|  |_|\_ \__|\__, |\__, |                                                                                                                    
-           |___/|_|                                    |___/ |___/                                                                                                                     
+| |/ /_ __ _   _ _ __ | |_    __   _____ _ __| | _| |_ _   _  __ _ 
+| ' /| '__| | | | '_ \| __|___\ \ / / _ \ '__| |/ / __| | | |/ _` |
+| . \| |  | |_| | |_) | ||_____\ V /  __/ |  |   <| |_| |_| | (_| |
+|_|\_\_|   \__, | .__/ \__|     \_/ \___|_|  |_|\_ \__|\__, |\__, |
+           |___/|_|                                    |___/ |___/ 
  ____   ___   ___   ___
 | ___| / _ \ / _ \ / _ \ 
 |___ \| | | | | | | | | |
@@ -18,29 +18,31 @@ parser = argparse.ArgumentParser(description=r"""
 
                                  
 Änvändning:
-                    py SCRIPT.py -f FILNAMN NYCKEL -e/d
+                    py SCRIPT.py -f FILNAMN -k NYCKEL -e/d
 
                                  
 Flaggor:
 -c  --create_key    Skapa en ny krypteringsnyckel och spara den som en ny fil.
                                                           
--f  --files         Ange både filen du vill kryptera/dekryptera och nyckefilen i den givna ordningen.
-                    Notera: Ange alltid filnamn först och sedan nyckelfil.
+-f  --files         Ange filen du vill kryptera/dekryptera.
+                    Notera: Krypterar inte '.enc'.filer och dekrypterar inte annat än '.enc' filer.
                                  
 -e  --encrypt       Välj '-e' för att kryptera den valda filen med den valda nyckelfilen
-                    Notera: En krypterad kopia med filtyp 'FILNAMN.FILTYP.enc' skapas i den aktuella sökvägen
+                    Notera: En krypterad kopia med filtyp 'FILNAMN.FILTYP.enc' skapas i den aktuella sökvägen.
                                  
--d  --decrypt       Välj '-d' för att dekryptera den valda filen med den valda nyckelfilen
-                    Notera: En dekrypterad kopia med filanamn 'dekrypterad_FILNAMN.FILTYP' skapas i den aktuella sökvägen                    
+-d  --decrypt       Välj '-d' för att dekryptera den valda filen med den valda nyckelfilen.
+                    Notera: En dekrypterad kopia med filanamn 'dekrypterad_FILNAMN.FILTYP' skapas i den aktuella sökvägen.
+                                                   
+-k  --key           Ange nyckelfilen du vill använda för att kryptera/dekryptera.
 
                                  
 Exempel på korrekta användningar:
 
-Skapa ny nyckel:    py slutprojv2.py -c ny_nyckel.key
+Skapa ny nyckel:    py fsh_Amos_kv.py -c ny_nyckel.key
 
-Kryptera en fil:    py slutprojv2.py -f testfil.txt test.key -e
+Kryptera en fil:    py fsh_Amos_kv.py -f testfil.txt -k test.key -e
 
-Dekryptera en fil:  py slutprojv2.py -f testfil.txt test.key -d""", 
+Dekryptera en fil:  py fsh_Amos_kv.py -f testfil.txt -k test.key -d""", 
 epilog="Information finns ovan", formatter_class=argparse.RawDescriptionHelpFormatter)   
 
 #### funktion för att skapa nyckel
@@ -83,8 +85,8 @@ def main():
 
 #### argument
     parser.add_argument("-c", "--create_key", metavar="Skapa ny nyckel", help="Skapa ny krypterinsnyckel")
-    parser.add_argument("-f", "--files", metavar="Välj: FIL", type=str, help="Ange fil att kryptera/dekryptera") 
-    parser.add_argument("-k", "--key", metavar="Välj: NYCKEL", type=str, help="Ange nyckelfil för kryptera/dekryptera fil") 
+    parser.add_argument("-f", "--file", metavar="Välj: FIL ", type=str, help="Ange fil att kryptera/dekryptera") 
+    parser.add_argument("-k", "--key", metavar="Välj: NYCKEL ", type=str, help="Ange nyckelfil för kryptera/dekryptera fil") 
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-e", "--encrypt", action="store_true", help="Välj för kryptering")
@@ -103,15 +105,17 @@ def main():
         else:
             print(f"Nyckel {args.create_key} finns redan")
 
-    if (args.files and args.key) and not args.encrypt or args.decrypt:
-        print("Fel: Du behöver ange krypteringsläge '-e' eller '-d'") 
+#### kontroll
+    if (args.file and args.key) and not (args.encrypt or args.decrypt):
+        print("Fel: Du behöver ange krypteringsläge '-e' eller '-d'")
+        return
 
 #### check av fil
-    if args.files and (args.decrypt or args.encrypt):
-        if not os.path.exists(args.files):
-            print(f"Fil {args.files}finns ej")
+    if args.file and (args.decrypt or args.encrypt):
+        if not os.path.exists(args.file):
+            print(f"Fil {args.file}finns ej")
             return
-        excisting_file = args.files
+        excisting_file = args.file
     else:
         print("Fel: ingen fil angiven. Ange '-f' och fil")
         return
@@ -140,7 +144,6 @@ def main():
         else:
             decrypt_and_store_info(excisting_file, excisting_key)
             print(f"Filen {excisting_file} är dekrypterat")
-
 
 
 
